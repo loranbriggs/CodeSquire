a5e91e4493ffb51db9d4cf66cd3742ff7686d2f7
Loran Briggs <loranbriggs@gmail.com>
2013-02-18 02:07:47 -0800

    rename php folder to api

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
 <a href="<?php echo $ROOT ?>/html5/">HTML 5 Games</a> |
 <a href="<?php echo $ROOT ?>/ror/">Ruby on Rails</a> |
-<a href="<?php echo $ROOT ?>/php/">PHP</a> |
+<a href="<?php echo $ROOT ?>/api/">APIs</a> |
 <a href="<?php echo $ROOT ?>/wordpress/">WordPress</a> |
 <a href="<?php echo $ROOT ?>/win8/">Windows 8 apps</a> |
 <a href="<?php echo $ROOT ?>/java/">Java</a> |
