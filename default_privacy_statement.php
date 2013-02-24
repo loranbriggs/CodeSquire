8339a43ddf59ce3a96e47a7ae221166eac98cd9e
Loran Briggs <loranbriggs@gmail.com>
2013-02-24 02:18:27 -0800

    add privacey statement page

---
+++ default_privacy_statement.php
@@ -0,0 +1,7 @@
+<?PHP include 'head.php'; ?>
+  <h1 class="text-center">Privacey Statement</h1>
+  <p class="hero-unit">
+    This application does not collect or store any of your personal infomation.<br>
+    Direct any questions to <a href="info@codesquire.com">info@codesquire.com</a>
+  </p>
+<?PHP include 'foot.php'; ?>
