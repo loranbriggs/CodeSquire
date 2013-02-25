67496bc8717d9b0f1fca38c5cb38073a3feea494
Loran Briggs <loranbriggs@gmail.com>
2013-02-25 14:39:46 -0800

    update privacy statement

--- default_privacy_statement.php
+++ default_privacy_statement.php
@@ -1,7 +1,27 @@
 <?PHP include 'head.php'; ?>
-  <h1 class="text-center">Privacey Statement</h1>
-  <p class="hero-unit">
-    This application does not collect or store any of your personal infomation.<br>
-    Direct any questions to <a href="info@codesquire.com">info@codesquire.com</a>
-  </p>
+<h3>Privacy Policy</h3>
+<p>I hope the following statements will help you understand how this app will collect, use and protect your information.
+It will not use or share your information with anyone except as described in this Privacy Policy.</p>
+
+<h3>Please uninstall this APP if you do not agree with this privacy statement. </h3>
+<h3>What information is collected?</h3>
+<p> This app does not collect personal information outside of the analytics collected by the Windows Store which is called the UDID and IP Addresses which are shared with Microsoft PubCenter as shown below.
+No other information is collected. It does collect data from twitter but does not save any new data from you the user. </p>
+
+<h3>How is your information used?</h3>
+<p>If there is an advertisement banner, then the UDID information is collected in order to determine the aggregate number of unique devices using the APP, to track total usage, analyze data, and communicate with you more effectively.
+Also, the UDID and IP address are used to show the in-APP advertisement. </p>
+
+<h3>How is your information shared?</h3>
+<p> Both the UDID and IP address are shared with Microsoft PubCenter. This is needed to show the in-APP advertisement.
+Other information may be collected with further communications with you.</p>
+
+<p>This app does not collect any information about you or any users, except analytics data as described below.</p>
+
+<h3>Analytics</h3>
+<p>Analytics data is collected by Microsoft Research, as described in <a href="http://research.microsoft.com/en-us/UM/legal/TouchDevelopPS_4-2012%20v2.htm">Privacy Statement for TouchDevelop</a>. 
+Analytics data collection cannot be disabled. Microsoft does not store any information that personally identifies you.</p>
+
+<h3>Questions?</h3>
+<p>Any questions about this privacy policy use the address supplied in the support page </p>
 <?PHP include 'foot.php'; ?>
