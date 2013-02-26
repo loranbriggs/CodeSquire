1f3ae61aea8d0d8fb13447ed277dfd90155cfba2
Loran Briggs <loranbriggs@gmail.com>
2013-02-26 03:24:57 -0800

    fix minor bug

--- blog/index.php
+++ blog/index.php
@@ -1,5 +1,5 @@
 <?PHP include '../head.php'; ?>
-<a href="HelloC++/">Hello C++ XAML</a> |
-<a href="PrivacyStatementJavascript/">Privacy Statement in Windows 8 Javascript App</a> |
+<a href="HelloC++/">Hello C++ XAML</a> | |
+<a href="PrivacyStatementJavascript/">Privacy Statement in Windows 8 Javascript App</a> | |
 <a href="HelloC++Images/">Insert Images in C++ Programatically</a>
 <?PHP include(ROOT.'/foot.php'); ?>
