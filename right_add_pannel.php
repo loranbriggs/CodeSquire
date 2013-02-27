1e03b45ffec621035d1e5e1095efbd141c7a00fc
Loran Briggs <loranbriggs@gmail.com>
2013-02-26 23:42:19 -0800

    add ad banner

---
+++ right_add_pannel.php
@@ -0,0 +1,11 @@
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-6307932957540580";
+/* vertical-banner */
+google_ad_slot = "5049147759";
+google_ad_width = 160;
+google_ad_height = 600;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
