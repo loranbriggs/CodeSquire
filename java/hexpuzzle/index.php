b1e06309a8bb5c1545e1cfc2b44cf1bda9054b47
Loran Briggs <loranbriggs@gmail.com>
2013-03-14 14:33:08 -0700

    add hex puzzle

---
+++ java/hexpuzzle/index.php
@@ -0,0 +1,22 @@
+<?PHP include '../../head.php'; ?>
+  <div class='row-fluid'>
+    <div id='main-pannel' class='span10'>
+      <applet
+        class = "well"
+        code = "HexPuzzle.class"
+        width = 350
+        height = 650
+        align = "center"
+        alt = "error"
+      >
+      </applet>
+      <p class = "well">
+        Download the source code 
+        <a href="HexPuzzle.zip">Here</a>
+      </p>
+    </div>
+    <div id='right-pannel' class='span2'>
+      <?PHP include(ROOT.'/right_add_pannel.php'); ?>
+    </div>
+  </div>
+<?PHP include(ROOT.'/foot.php'); ?>
