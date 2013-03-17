ac7204024db83a2833929741ad8d745f7a2adee4
Loran Briggs <loranbriggs@gmail.com>
2013-03-17 11:32:53 -0700

    updated html page

--- html5/index.php
+++ html5/index.php
@@ -8,9 +8,21 @@
   -->
   <div class='row-fluid'>
     <div id='main-pannel' class='span10'>
-      <div class="text-center">
-        <a href="rougeassassin/"><h2>Rouge Assassin</h2>
-        <img src="rougeassassin/images/startbg.png" /></a>
+      <div class="tabbable"> <!-- Only required for left/right tabs -->
+        <ul class="nav nav-tabs">
+          <li class="active"><a href="#tab1" data-toggle="tab">Rogue Assassin</a></li>
+          <li><a href="#tab2" data-toggle="tab">Smoke and Fire</a></li>
+        </ul>
+        <div class="tab-content">
+          <div class="tab-pane active" id="tab1">
+            <a href="rougeassassin/"><h2>Rouge Assassin</h2>
+            <img src="rougeassassin/images/startbg.png" /></a>
+          </div>
+          <div class="tab-pane" id="tab2">
+            <a href="../smokeandfire/"><h2>Smoke and Fire</h2>
+            <img src="../smokeandfire/images/croppedscreenshots.png" /></a>
+          </div>
+        </div>
       </div>
     </div>
     <div id='right-pannel' class='span2'>
