f69538f16b26a557f432e3eade090f4415914d7b
Loran Briggs <loranbriggs@gmail.com>
2013-09-18 12:19:25 -0700

    add reveal images csv

---
+++ reveal/index.php
@@ -0,0 +1,40 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $imageId =  $_GET['imageId'];
+        $link = $_GET['link'];
+        $deleteHash = $_GET['deleteHash'];
+
+
+        $fp = fopen('images.csv', 'a');
+        fputcsv($fp, array($imageId, $link, $deleteHash));
+        fclose($fp);
+    }
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Save Image Hash</title>
+  </head>
+
+    <body>
+        Images:
+        <?php
+            $row = 1;
+            if (($handle = fopen("images.csv", "r")) !== FALSE) {
+                while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
+                    $num = count($data);
+                    echo "<p> $num fields in line $row: <br /></p>\n";
+                    $row++;
+                    for ($c=0; $c < $num; $c++) {
+                        echo $data[$c] . "<br />\n";
+                    }
+                }
+                fclose($handle);
+            }
+        ?>
+   </body>
+
+</html>
